d49dfee8dc387378ca631b62c43fd1e3705a20f6
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-07-19 16:31:59 -0400

    draft of correlation matrix for calapooia, start on distribution figures.

---
+++ distribution_plots.R
@@ -0,0 +1,21 @@
+# Packages
+library(tidyverse)
+library(here)
+library(stringr)
+library(hrbrthemes)
+library(ggjoy)
+
+wi <- unique(read_csv(here("data/watershed_integrity_new.csv")))
+
+# Calapooia
+cal_id <- names(wi)[1:4]
+cal_y <- c("logNdif","dN15chironimid","total_in","xcmgw","xembed","fishMMI",
+           "max_tempC_summer","phase")
+
+cal_x <- c("iwi", "ici","wchem","cchem","whabt","chabt","wsed","csed","whyd",
+           "chyd","wtemp","ctemp")
+
+cal <- wi %>%
+  filter(watershed == "calapooia") %>%
+  spread(key = variable, value = value) %>%
+  select(c(cal_id,cal_y,cal_x))
