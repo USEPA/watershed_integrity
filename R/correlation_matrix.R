8cc3f9618ad295400328be9b99e525cbe2310d16
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-07-21 11:28:10 -0400

    cleaned up correlation matrix

--- R/correlation_matrix.R
+++ R/correlation_matrix.R
@@ -19,26 +19,36 @@ cal <- wi %>%
   spread(key = variable, value = value) %>%
   select(c(cal_id,cal_y,cal_x))
 
-cal_cor <- cor(cal[,5:24],use = "pairwise.complete.obs") %>%
+cal_cor_df <- cor(cal[,5:24],use = "pairwise.complete.obs") %>%
   data.frame() %>%
   rownames_to_column("index") %>% 
-  filter(str_detect(index, 'iwi|ici|whyd|chyd|wchem|cchem|wsed|csed|wconn|cconn|wtemp|ctemp|whabt|chabt')) %>%
-  select(c("index",cal_y)) %>%
+  filter(str_detect(index, 'iwi|ici|whyd|chyd|wchem|cchem|wsed|csed|wconn|cconn|wtemp|ctemp|whabt|chabt'))  %>%
+  select(c("index",cal_y))
+         
+write_csv(round(cal_cor_df, 3), path = "data/cal_correlations.csv")
+
+cal_cor <- cal_cor_df  %>%
   gather("variable", "value", -1)
 
 cal_cor_gg <- ggplot(cal_cor, aes(x = index, y = variable)) +
   geom_point(aes(size = abs(value), color = value)) + 
-  scale_color_viridis_c(name = "Pearson\nCorrelation", limits = c(-1,1), 
-                        breaks = c(-1,-0.5,0,0.5,1)) +
+  scale_color_viridis_c(name = "Pearson\nCorrelation", 
+                        limits = round(range(cal_cor$value),2), 
+                        breaks = round(c(max(cal_cor$value),0.5,0,-0.5,min(cal_cor$value)),2),
+                        guide = guide_legend(
+                          override.aes = list(color = viridis::viridis(5,direction = -1), 
+                                              size = c(6,3,1,3,6)),
+                          reverse = FALSE)) +
   scale_size(range = c(1,6)) +
   theme_ipsum() +
   scale_x_discrete(position = "top") +
   labs(x = "", y = "") +
   theme(legend.text = element_text(size = 10),
         axis.text.x = element_text(angle= 45, hjust = 0)) +
-  guides(color = guide_legend(override.aes = 
-                         list(color = viridis::viridis(5,direction = 1),
-                              size = c(6,3,1,3,6))),
+  guides(color = guide_legend(
+    override.aes = list(color = viridis::viridis(5,direction = -1), 
+                        size = c(6,3,1,3,6)),
+    reverse = FALSE),
          size = FALSE)
 
 cal_cor_gg
