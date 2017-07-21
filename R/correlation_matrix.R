b66ed2ae84e7836a3a77cb3bc3dc698df560ad19
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-07-21 12:35:57 -0400

    draft done, now to functionize!

--- R/correlation_matrix.R
+++ R/correlation_matrix.R
@@ -25,10 +25,11 @@ cal_cor_df <- cor(cal[,5:24],use = "pairwise.complete.obs") %>%
   filter(str_detect(index, 'iwi|ici|whyd|chyd|wchem|cchem|wsed|csed|wconn|cconn|wtemp|ctemp|whabt|chabt'))  %>%
   select(c("index",cal_y))
          
-write_csv(data.frame(cal_cor[,1],round(cal_cor_df[,-1], 3)), path = "data/cal_correlations.csv")
+write_csv(data.frame(index = cal_cor_df[,1],round(cal_cor_df[,-1], 3)), path = "data/cal_correlations.csv")
 
 cal_cor <- cal_cor_df  %>%
-  gather("variable", "value", -1)
+  gather("variable", "value", -1) %>%
+  mutate(value = round(value,2))
 
 cal_cor_gg <- ggplot(cal_cor, aes(x = index, y = variable)) +
   geom_point(aes(size = abs(value), color = value)) + 
@@ -36,20 +37,15 @@ cal_cor_gg <- ggplot(cal_cor, aes(x = index, y = variable)) +
                         limits = round(range(cal_cor$value),2), 
                         breaks = round(c(max(cal_cor$value),0.5,0,-0.5,min(cal_cor$value)),2),
                         guide = guide_legend(
-                          override.aes = list(color = viridis::viridis(5,direction = -1), 
-                                              size = c(6,3,1,3,6)),
+                          override.aes = list(#color = viridis::viridis(5,direction = -1), 
+                                              size = c(5,2.5,1,2.5,5)),
                           reverse = FALSE)) +
-  scale_size(range = c(1,6)) +
+  scale_size(range = c(1,5), guide = FALSE) +
   theme_ipsum() +
   scale_x_discrete(position = "top") +
   labs(x = "", y = "") +
   theme(legend.text = element_text(size = 10),
-        axis.text.x = element_text(angle= 45, hjust = 0)) +
-  guides(color = guide_legend(
-    override.aes = list(color = viridis::viridis(5,direction = -1), 
-                        size = c(6,3,1,3,6)),
-    reverse = FALSE),
-         size = FALSE)
+        axis.text.x = element_text(angle= 45, hjust = 0))
 
 cal_cor_gg
 ggsave(filename = "cal_cor_fig.jpg", plot = cal_cor_gg, width = 7, height = 5, 
