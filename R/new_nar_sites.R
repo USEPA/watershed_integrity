5705b21a8e51969611f72ff6cb9dcd2aa4f2f654
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-07-28 12:20:15 -0400

    starting on update of nar sites

--- R/new_nar_sites.R
+++ R/new_nar_sites.R
@@ -1,5 +1,15 @@
 library(tidyverse)
 library(here)
 
+wi <- unique(read_csv(here("data/watershed_integrity_new.csv")))
+nar_miss <- read_csv(here("data/raw/NAR_missing streamsites.csv"))
+nar_stream_sites <- read_csv("data/raw/NAR_comid_stream siteID.csv")
 
-nar_miss <- read_csv("data/raw/NAR_missing streamsites.csv")
+nar_streams_long2 <- nar_miss %>%
+  left_join(nar_stream_sites, by = c("comid")) %>%
+  select(comid,watershed,site, everything()) %>%
+  gather(variable, value, 5:30) %>%
+  rename(waterbody_type = `waterbody type`) %>%
+  mutate(watershed = "narragansett bay")
+
+
