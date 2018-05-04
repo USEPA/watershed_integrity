1b3ab1a6df214a61e8f3839202df47ffe4f4411f
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-05-04 10:20:15 -0400

    updated figures

--- R/distribution_plots.R
+++ R/distribution_plots.R
@@ -7,14 +7,16 @@ library(ggridges)
 library(forcats)
 
 # Data munging
-wi <- unique(read_csv(here("data/watershed_integrity_new_2.csv")))
+wi <- unique(read_csv(here("data/watershed_integrity_new_2.csv"))) %>%
+  mutate(variable = toupper(variable))
 landscape <- read_csv(here("data/raw/lulc_4wtsheds.csv")) %>%
   select(-watershed) %>%
   gather(variable, value, 2:7) %>%
   left_join(select(wi,comid,watershed,site,waterbody_type)) %>%
   select(comid, watershed, site, waterbody_type, variable, value) %>%
   unique() %>%
-  mutate(value = value/100)
+  mutate(value = value/100,
+         variable = stringr::str_to_title(variable))
 
 wi <- wi %>%
   rbind(landscape) %>%
@@ -22,17 +24,17 @@ wi <- wi %>%
                                                   "East Fork\nLittle Miami", 
                                                   "Narragansett\nBay")))
 
-index_vars <- c("iwi", "wtemp","wsed","whyd","whabt","wconn","wchem",
-                "pctforestws","pctagws","pcturbanws",
-                "ici","ctemp","csed","chyd","chabt","cconn","cchem",
-                "pctforestcat","pctagcat","pcturbancat")
+index_vars <- c("IWI", "WTEMP","WSED","WHYD","WHABT","WCONN","WCHEM",
+                "Pctforestws","Pctagws","Pcturbanws",
+                "ICI","CTEMP","CSED","CHYD","CHABT","CCONN","CCHEM",
+                "Pctforestcat","Pctagcat","Pcturbancat")
 index_vars <- index_vars[length(index_vars):1]
 
 
 # Joy plots of distribution of all index components by study area
 wi_index <- wi %>%
-  filter(toupper(variable) %in% toupper(index_vars)) %>%
-  mutate(variable = fct_relevel(toupper(variable), toupper(index_vars)))
+  filter(variable %in% index_vars) %>%
+  mutate(variable = fct_relevel(variable, index_vars))
 
 index_all_ws_gg <- ggplot(wi_index, aes(x = value, y = variable)) +
   geom_density_ridges(rel_min_height = 0.001, scale = 0.9, bandwidth = 0.04) +
@@ -45,7 +47,9 @@ index_all_ws_gg <- ggplot(wi_index, aes(x = value, y = variable)) +
         axis.text.y = element_text(size = 12),
         panel.spacing.x = unit(0.25, "cm"),
         plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm"))
-index_all_ws_gg
 
-ggsave(filename = here("figures/iwi_ici_joy_ws_fig.tiff"), plot = index_all_ws_gg, width = 6, height = 5, 
-       units = "in", dpi = 300)
+tiff(filename = here("figures/iwi_ici_joy_ws_fig.tiff"), width = 5, height = 5, 
+     units = "in", pointsize = 12, res=1000, compression="lzw")
+index_all_ws_gg
+dev.off()  # This closes the tiff device after drawing the fig
+
